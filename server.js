51fd327cc05d933b67d731de77ce25978ca439e8
Manish Atri <atri.manish.iiita@gmail.com>
2016-02-24 14:12:23 +0530

    Refactor api with underscore

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser =require('body-parser');
+var _= require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -23,7 +25,9 @@ app.get('/todos', function (req,res) {
 app.get('/todos/:id', function (req,res){
 	
 	var todoId = parseInt(req.params.id);
-	var matchedTodo;
+	var matchedTodo = _.findWhere(todos,{id:todoId} );
+	
+	/*var matchedTodo;
 	for (var i = 0; i <= todos.length - 1; i++) {
 		 	console.log("checking for id availablity")
 		 	 
@@ -31,7 +35,7 @@ app.get('/todos/:id', function (req,res){
 		 	 	matchedTodo =todos[i] ;
 		 	 			 	 }
 		 };
-
+*/
 		 if (matchedTodo) {
 		 	res.send(matchedTodo);
 		 } else{
@@ -43,18 +47,21 @@ app.get('/todos/:id', function (req,res){
 // POST /todos
 app.post('/todos',function (req,res) {
 	var body = req.body;
+	body = _.pick(body,'description','completed');
+	
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ) {
+		//console.log("isBoolean :" + _.isBoolean(body.completed)  + "isString : "+!_.isString(body.description) + "desc body len: " +  body.description.trim().length) ;
+		return res.status(400).send();
+	} 
+
+	body.description = body.description.trim();
+	console.log("no bad request, ");
 	body.id =todonextId++;
-	//body.description=req.description;
-
 	todos.push(body)
-
-
 	console.log('description' + body.description);
 	res.json(body);
 	// body...
 });
-
-
 
 
 app.listen(PORT, function(){
