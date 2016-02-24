3cbb7b205f882f2e4f14fc204b41bd5b5445fc3f
Manish Atri <atri.manish.iiita@gmail.com>
2016-02-24 19:38:50 +0530

    add put todos

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.post('/todos',function (req,res) {
 
 
 //DELETE /todos/:id
-app.delete('/todos/:id',function (req,res) {
+app.delete('/todos/:id', function (req,res) {
 
 	var deleteTodoId = parseInt(req.params.id, 10);
 	var matchedTodo = _.findWhere(todos,{id:deleteTodoId} );
@@ -80,6 +80,58 @@ app.delete('/todos/:id',function (req,res) {
 	res.json(matchedTodo);
 	
 });
+
+// PUT /todos/:id
+
+app.put('/todos/:id', function (req, res) {
+
+	console.log(" comes to update code");
+
+	var todoId = parseInt(req.params.id, 10);
+
+	var matchedTodo = _.findWhere(todos,{id:todoId} );
+
+	if (!matchedTodo) {
+		console.log("No Macthed todo");
+		return res.status(404).send();
+	} 
+
+	console.log(" Macthed todo");
+	var body = req.body;
+	body = _.pick(body,'description','completed');
+	var validAttributes ={};
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+
+		console.log(" updating completed property ");
+		validAttributes.completed = body.completed;
+	
+	} else if (body.hasOwnProperty('completed')) {
+		
+			return res.status(400).send();
+	} 
+    console.log(" coming to description part ");
+    
+	if ( body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		
+		console.log(" updating completed property ");
+		
+		validAttributes.description =body.description;
+
+	
+	} else if(body.hasOwnProperty('description')) {
+			return res.status(400).send();
+
+	}
+
+	console.log("updating the validAttributes");
+	
+	_.extend(matchedTodo,validAttributes);
+
+	res.json(matchedTodo);
+});
+
+
 
 
 app.listen(PORT, function(){
